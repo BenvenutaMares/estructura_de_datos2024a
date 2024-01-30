8ceaf1aaa780dffc1f081c51c1ad8aa82c7eae37
Samir <samirmares@gmail.com>
2024-01-30 08:58:12 -0600

    Bubu
    Tarea codigo burbuja

---
+++ desorden_orden.py
@@ -0,0 +1,18 @@
+#lista desordeb
+listamal = [2,1,3,4,5,3,9]
+
+#definir tamañp 
+longitud = len(listamal)
+for i in range(longitud):
+   # print ("Esto es i",i)prueba de que hace
+
+    for j in range(longitud-1):
+        #print("Esto es j",j) prueba de que se mueve
+        #Comparacion 
+        if listamal[j] > listamal[j+1]:
+                #intercambio
+                            listamal[j], listamal[j+1] = listamal[j+1], listamal[j]
+            
+
+
+print(listamal)
